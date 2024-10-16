c5f4fa02cc34d1942a1a4295f220ba4546771c31
Mathis Hoziaux <92360510+HozMath@users.noreply.github.com>
2024-10-16 17:49:21 +0200

    Update button.js

--- src/button.js
+++ src/button.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -18,11 +18,7 @@ export default function Board() {
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
@@ -35,8 +31,15 @@ export default function Board() {
     status = "Prochain tour : " + (xIsNext ? "X" : "O");
   }
 
+  // Fonction pour redémarrer la partie
+  function handleReload() {
+    setSquares(Array(9).fill(null)); // Réinitialiser les carrés
+    setXIsNext(true); // Réinitialiser le joueur suivant à X
+  }
+
   return (
     <>
+      <div className='title'><h1>Tic Tac Toe</h1></div>
       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -53,26 +56,29 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+      <div className="reload">
+        <button onClick={handleReload}>Recommencer</button>
+      </div>
     </>
   );
 }
 
 function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null;
   }
+  return null;
+}
