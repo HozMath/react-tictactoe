17fdcd7edb7efcf8f1335083a52317259eddd268
Mathis Hoziaux <92360510+HozMath@users.noreply.github.com>
2024-10-16 17:50:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
 import Button from './button';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
     <div class="blockTic">
       <div class="tictoctoe">
         <Button />
